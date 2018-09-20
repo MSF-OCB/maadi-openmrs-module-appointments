08ff961acfd8219c72748db4e2fa2c5beb316864
sowmika <sowmika148@gmail.com>
2018-09-20 12:29:36 +0530

    Sowmika, Raju | [Weekly View] | Remove Adding 1 day for endDate(Handled in frontEnd by passing end of day)

--- omod/src/main/java/org/openmrs/module/appointments/web/controller/AppointmentsController.java
+++ omod/src/main/java/org/openmrs/module/appointments/web/controller/AppointmentsController.java
@@ -3,10 +3,14 @@ package org.openmrs.module.appointments.web.controller;
 import org.openmrs.module.appointments.model.Appointment;
 import org.openmrs.module.appointments.model.AppointmentSearch;
 import org.openmrs.module.appointments.service.AppointmentsService;
+import org.openmrs.module.appointments.util.DateUtil;
 import org.openmrs.module.appointments.web.contract.AppointmentDefaultResponse;
 import org.openmrs.module.appointments.web.mapper.AppointmentMapper;
 import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.RestUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +19,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.validation.Valid;
+import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import static java.util.Objects.isNull;
 
